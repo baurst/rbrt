6430ba228694fb4e9c1988e662e9f04c4201b08a
baurst <stefan.a.baur@gmail.com>
2019-09-08 12:57:08 +0200

    first box that makes sense
    Branch: master

--- src/main.rs
+++ src/main.rs
@@ -107,7 +107,7 @@ fn main() {
         corner_a: Vec3::new(3.0, 2.0, -4.0),
         corner_c: Vec3::new(3.0, 3.0, -4.0),
         material: Box::new(Lambertian {
-            albedo: Vec3::new(0.5, 0.2, 0.2),
+            albedo: Vec3::new(0.9, 0.2, 0.2),
         }),
     });
 
@@ -118,18 +118,18 @@ fn main() {
     let cube = tobj::load_obj(&Path::new("cube.obj"));
     // todo: fix this hacked filepath!
     assert!(cube.is_ok());
-    let (models, _materials) = cube.unwrap();
+    let (models, materials) = cube.unwrap();
 
-    let transl = Vec3::new(3.0, 3.0, -7.0);
+
 
     println!("# of models: {}", models.len());
+    println!("# of materials: {}", materials.len());
     for (i, m) in models.iter().enumerate() {
         let mesh = &m.mesh;
+        println!("model[{}].name = \'{}\'", i, m.name);
+        println!("model[{}].mesh.material_id = {:?}", i, mesh.material_id);
+
         println!("Size of model[{}].indices: {}", i, mesh.indices.len());
-        // Normals and texture coordinates are also loaded, but not printed in this example
-        println!("model[{}].vertices: {}", i, mesh.positions.len() / 3);
-        assert!(mesh.positions.len() % 3 == 0);
-        let mut triangle_vertices: Vec<Vec3> = vec![Vec3::zero(); 3];
         for f in 0..mesh.indices.len() / 3 {
             println!(
                 "    idx[{}] = {}, {}, {}.",
@@ -138,25 +138,57 @@ fn main() {
                 mesh.indices[3 * f + 1],
                 mesh.indices[3 * f + 2]
             );
+        }
+
+        // Normals and texture coordinates are also loaded, but not printed in this example
+        println!("model[{}].vertices: {}", i, mesh.positions.len() / 3);
+        assert!(mesh.positions.len() % 3 == 0);
+        for v in 0..mesh.positions.len() / 3 {
+            println!(
+                "    v[{}] = ({}, {}, {})",
+                v,
+                mesh.positions[3 * v],
+                mesh.positions[3 * v + 1],
+                mesh.positions[3 * v + 2]
+            );
+        }
+    }
+    let transl = Vec3::new(-2.0, 3.0, -7.0);
+    let scale = 1.0;
+
+    for (i, m) in models.iter().enumerate() {
+
+        let mesh = &m.mesh;
+        // Normals and texture coordinates are also loaded, but not printed in this example
+        assert!(mesh.positions.len() % 3 == 0);
+        let mut triangle_vertices: Vec<Vec3> = vec![Vec3::zero(); 3];
+        for f in 0..mesh.indices.len() / 3 {
+
+
             for idx in 0..3 {
+                let x_idx = 3* mesh.indices[3 * f + idx];
+                let y_idx = 3* mesh.indices[3 * f + idx] + 1;
+                let z_idx = 3* mesh.indices[3 * f + idx] + 2;
+                
                 triangle_vertices[idx] = Vec3::new(
-                    mesh.positions[(mesh.indices[3 * f + idx] + 0) as usize] as f64,
-                    mesh.positions[(mesh.indices[3 * f + idx] + 1) as usize] as f64,
-                    mesh.positions[(mesh.indices[3 * f + idx] + 2) as usize] as f64,
+                    mesh.positions[x_idx as usize] as f64 * scale,
+                    mesh.positions[y_idx as usize] as f64 * scale,
+                    mesh.positions[z_idx as usize] as f64 * scale
                 );
-                println!("    idx[{}] = {:?} ", 3 * f + idx, triangle_vertices[idx]);
+                println!("    triangle {}, corner {} = {:?} ", f, idx, triangle_vertices[idx]);
             }
             let tri = Box::new(Triangle {
                 corner_a: triangle_vertices[0] + transl,
                 corner_b: triangle_vertices[1] + transl,
                 corner_c: triangle_vertices[2] + transl,
-                material: Box::new(Lambertian {
-                    albedo: Vec3::new(0.2, 0.2, 0.8),
-                }),
+                material: Box::new(Metal {
+                    albedo: Vec3::new(0.0, 0.2, 0.9),
+                fuzz: 0.01}),
             });
             model_elements.push(tri);
         }
     }
+
 
     println!("# of model_elements: {}", model_elements.len());
 
